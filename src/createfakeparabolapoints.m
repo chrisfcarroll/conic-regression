ac045c5c345166758a4b405df45afc1d46a4ce39
chrisfcarroll <chris.carroll@unforgettable.me.uk>
2016-12-07 01:46:35 +0000

    -m fits for non-rotated parabolas and can show rotated parabolas

--- src/createfakeparabolapoints.m
+++ src/createfakeparabolapoints.m
@@ -2,11 +2,13 @@
 % oriented approximately facing downwards-right with axis of symmetry
 % at angle around -30 degrees down from the x-axis
 %
+clc
 topleft= [      rand(100) 
           500 + rand(100)];
 
 minangle= -0.3; maxangle= -0.7;
 theta=- minangle + rand() * (maxangle-minangle);
+theta=pi/4;
 rot=[cos(theta)  -sin(theta) 0 
      sin(theta)   cos(theta) 0
          0            0      1];
@@ -22,14 +24,16 @@ Q1= [a  0  b
 quadratic= @(xy, M)([xy 1] * M * [xy 1]');
 rotatedquadratic= @(xy, M, rot)([xy 1] * rot' * M * rot * [xy 1]');
 
-for x= -10:10
-  y= -x^2;
-  xyR= [x y 1] * rot
-  assert( rotatedquadratic ( ([-3 -9 1] * rot)(1:2) , Q1, rot ), 0, 1e14);
-endfor
+% for x= -10:10
+%   y= -x^2;
+%   xyR= [x y 1] * rot;
+%   assert( rotatedquadratic ( ([-3 -9 1] * rot)(1:2) , Q1, rot ), 0, 1e14);
+% endfor
 
 
 input= [
+  -3  -9
+  -2  -4
   -1  -1
   0  0
   1  -1
@@ -39,24 +43,30 @@ input= [
   5  -25
   ];
 
+input(:,2) = input(:,1) + input(:,2);
+
 xyR= [input ones(rows(input),1)] * rot;
 xR= xyR(:,1);
-features= [xR xR.^2 ones(rows(xR),1)];
+yR= xyR(:,2);
+features= [xR xR.^2 ones(rows(xR),1) ];
 
 
 m = length(input);             % number of points
 %X = [ones(m,1), input(:,1)];   % forming X of X beta = y
 %y = input(:,2);                % forming y of X beta = y
 X= features;
-y= xyR(:,2);
+y= yR;
 
-betaHat = (X' * X) \ X' * y;   % computing projection of matrix X on y, giving beta
+betaH = (X' * X) \ X' * y;   % computing projection of matrix X on y, giving beta
 % display best fit parameters
-disp(betaHat);
+disp(betaH);
 
-xx = linspace(-1, 5, 20);
-yy = betaHat(1) + betaHat(2)*xx + betaHat(3) * xx.^2;
-plot(xx, yy) % plot the best fit line
+xx = linspace(-4, 7, 20);
+yy = betaH(1)*xx + betaH(2) * xx.^2 + betaH(3); % seems to be backwards?
+
+plot(xx, yy, '-co') % plot the best fit line
 hold on
-plot(input(:,1), input(:,2), 'or') % data for which we found the best fit
+% plot(input(:,1), input(:,2), '--yo') % data for which we found the best fit
+plot(xyR(:,1), xyR(:,2), '--k+') % data for which we found the best fit
+
 hold off
