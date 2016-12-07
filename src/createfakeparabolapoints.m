e31f65317246db9fb71127e37b63ea8abdb8fbc2
chrisfcarroll <chris.carroll@unforgettable.me.uk>
2016-12-07 16:19:25 +0000

    single function demo of quadratic regression and of rotated quadratic

--- src/createfakeparabolapoints.m
+++ src/createfakeparabolapoints.m
@@ -1,72 +1,81 @@
-% Create fake data for points approximately on a parabola, 
-% oriented approximately facing downwards-right with axis of symmetry
-% at angle around -30 degrees down from the x-axis
+% Create fake data for points approximately on a parabola which is at an angle theta from 'straight up'
 %
-clc
-topleft= [      rand(100) 
-          500 + rand(100)];
 
-minangle= -0.3; maxangle= -0.7;
-theta=- minangle + rand() * (maxangle-minangle);
-theta=pi/4;
-rot=[cos(theta)  -sin(theta) 0 
-     sin(theta)   cos(theta) 0
-         0            0      1];
+theta= pi/5; % given theta as angle to rotate by
 
-rot2=[cos(theta/2)  -sin(theta/2) 
-     sin(theta/2)   cos(theta/2)];
+% Matrix magic: This matrix represents a rotation through angle Theta.
+rotateByThetaMatrix= [cos(theta)  -sin(theta) 0 
+                      sin(theta)   cos(theta) 0
+                          0            0      1];
 
+%------------------------------------------------------------------------------
+%
+% Example: y = -x^2 + C
+%
+% Create a sample list of (x, y= -x^2, constant) points 
+inputXYs= [
+  -3  -9  1
+  -2  -4  1
+  -1  -1  1
+   0   0  1
+   1  -1  1
+   2  -4  1
+   3  -9  1
+   4  -16 1
+   5  -25 1
+  ];
+randomJitter=[ rand(length(inputXYs),2) ones(length(inputXYs),1)];
+
+for angle = [0 theta]
+
+  if(angle == 0)
+    rotated_xy= inputXYs + randomJitter;
+  else
+    % Rotate the [x y 1] vectors by Theta 
+    % which is done by the magic of matrix multiplication
+    rotated_xy= inputXYs * rotateByThetaMatrix;
+  endif
+
+  rotated_x= rotated_xy(:,1);
+  rotated_y= rotated_xy(:,2);
+
+
+  % Create features for regression learning as [rotated-x rotated-x^2]
+  features= [rotated_x rotated_x.^2 ones(rows(rotated_x),1) ];
+  X= features;
+  y= rotated_y;
+
+  % Solve it in one line
+  coefficients = (X' * X) \ X' * y; 
+
+
+  % Show the original points and the extrapolation
+  disp(coefficients);
+  figure(ifelse(angle==0, 1,2))
+  if(angle==0)
+    extrapolatedX = linspace(-4, 7, 20);
+    extrapolatedY = coefficients(1)*extrapolatedX + coefficients(2) * extrapolatedX.^2 + coefficients(3);
+    plot(extrapolatedX, extrapolatedY, '-co')
+    title('Black is original, blue is extrapolated')
+  else
+    title('Rotated quadratics aren''t that hard')
+  endif
+  hold on
+  plot(rotated_xy(:,1), rotated_xy(:,2), '-.k+') % data for which we found the best fit
+  hold off
+
+endfor
+
+
+%----------------------------------------------------------
+%
+% Aside: How to create a function that represents a rotated quadratic: (ax^2 + bx + c - y ) * rotateByThetaDegrees
+%
 a=-1; b=0; c=0;
 Q1= [a  0  b 
-         0  0 -1 
-         0  0  c];
-
-quadratic= @(xy, M)([xy 1] * M * [xy 1]');
-rotatedquadratic= @(xy, M, rot)([xy 1] * rot' * M * rot * [xy 1]');
-
-% for x= -10:10
-%   y= -x^2;
-%   xyR= [x y 1] * rot;
-%   assert( rotatedquadratic ( ([-3 -9 1] * rot)(1:2) , Q1, rot ), 0, 1e14);
-% endfor
-
-
-input= [
-  -3  -9
-  -2  -4
-  -1  -1
-  0  0
-  1  -1
-  2  -4
-  3  -9
-  4  -16
-  5  -25
-  ];
-
-input(:,2) = input(:,1) + input(:,2);
-
-xyR= [input ones(rows(input),1)] * rot;
-xR= xyR(:,1);
-yR= xyR(:,2);
-features= [xR xR.^2 ones(rows(xR),1) ];
-
-
-m = length(input);             % number of points
-%X = [ones(m,1), input(:,1)];   % forming X of X beta = y
-%y = input(:,2);                % forming y of X beta = y
-X= features;
-y= yR;
-
-betaH = (X' * X) \ X' * y;   % computing projection of matrix X on y, giving beta
-% display best fit parameters
-disp(betaH);
-
-xx = linspace(-4, 7, 20);
-yy = betaH(1)*xx + betaH(2) * xx.^2 + betaH(3); % seems to be backwards?
-
-plot(xx, yy, '-co') % plot the best fit line
-hold on
-% plot(input(:,1), input(:,2), '--yo') % data for which we found the best fit
-plot(xyR(:,1), xyR(:,2), '--k+') % data for which we found the best fit
-
-hold off
+     0  0 -1 
+     0  0  c];
+quadratic= @(xy, M)([xy 1] * M * [xy 1]'); % this calculates (ax^2 + bx + c - y)
+rotatedquadratic= @(xy, M, rotateByThetaMatrix)([xy 1] * rotateByThetaMatrix' * M * rotateByThetaMatrix * [xy 1]');
+%
+%----------------------------------------------------------
