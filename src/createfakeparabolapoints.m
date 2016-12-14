39c83c7967ca2998ce9105010e355b78ba06d72a
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2016-12-14 03:15:17 +0000

    parametric conic is incorrectly trying to match rotated quadratic as a cubic

--- src/createfakeparabolapoints.m
+++ src/createfakeparabolapoints.m
@@ -13,7 +13,7 @@ rotateByThetaMatrix= [cos(theta)  -sin(theta) 0
 % Example: y = -x^2 + C
 %
 % Create a sample list of (x, y= -x^2, constant) points 
-unrotatedXYs= [
+inputXYs= [
   -3  -9  1
   -2  -4  1
   -1  -1  1
@@ -24,16 +24,16 @@ unrotatedXYs= [
    4  -16 1
    5  -25 1
   ];
-randomJitter=[ rand(rows(unrotatedXYs),2) ones(rows(unrotatedXYs),1)];
+randomJitter= [ rand(length(inputXYs),2) ones(length(inputXYs),1)];
 
 for angle = [0 theta]
 
   if(angle == 0)
-    rotated_xy= unrotatedXYs + randomJitter;
+    rotated_xy= inputXYs + randomJitter;
   else
     % Rotate the [x y 1] vectors by Theta 
     % which is done by the magic of matrix multiplication
-    rotated_xy= unrotatedXYs * rotateByThetaMatrix;
+    rotated_xy= inputXYs * rotateByThetaMatrix;
   endif
 
   rotated_x= rotated_xy(:,1);
@@ -41,14 +41,9 @@ for angle = [0 theta]
 
 
   % Create features for regression learning as [rotated-x rotated-x^2]
-  features= [rotated_x      ...
-             rotated_x.^2   ...
-             rotated_y      ...
-             rotated_y.^2   ...
-             rotated_x .* rotated_y ...
-             ones(rows(rotated_x),1) ];
+  features= [rotated_x rotated_x.^2 ones(rows(rotated_x),1) ];
   X= features;
-  y= zeros(rows(rotated_x), 1);
+  y= rotated_y;
 
   % Solve it in one line
   coefficients = (X' * X) \ X' * y; 
--- src/parametricConic.m
+++ src/parametricConic.m
@@ -1,30 +1,18 @@
-% Create fake data for points approximately on a parabola which is at an angle theta from 'straight up'
-%
+%{ 
+------------------------------------------------------------------------------
+Create fake data for points approximately on a parabola which is at an angle theta from 'straight up'
 
-theta= pi/5; % given theta as angle to rotate by
-
-% Matrix magic: This matrix represents a rotation through angle Theta.
-rotateByThetaMatrix= [cos(theta)  -sin(theta) 0 
-                      sin(theta)   cos(theta) 0
-                          0            0      1];
-
-%------------------------------------------------------------------------------
-%{
 Example: 
-
 [x y 1]'  = [a  0  1 ;  * [t t^2 1]'
              0  b  1 ;
              0  0  1 ]
-
 and for the rotated case:
-
-[x y 1]'  = rotationMatrix * coefficentsMatrix  * [t t^2 1]'
+[x y 1]'  = rotationMatrix * coefficentsMatrix  * [t t^2 1]' 
 %}
+clear all;
 
-
-% let a=b=1
-
-unrotatedXYs= [
+unrotatedSourceData= [
+  -4  16  1
   -3   9  1
   -2   4  1
   -1   1  1
@@ -36,53 +24,60 @@ unrotatedXYs= [
    5  25  1
   ];
 
+% let a=b=1
 coefficients= ones(3);
+nRows=rows(unrotatedSourceData);
+randomJitter=[ rand(nRows,2) zeros(nRows,1)] - [ 0.5 * ones(nRows,2) zeros(nRows,1)];
 
-tfeatures= unrotatedXYs;
+for theta = [0  pi/20]
 
-randomJitter=[ rand(rows(tfeatures),2) ones(rows(tfeatures),1)];
+  rotateByThetaMatrix= [cos(theta)  -sin(theta) 0 
+                        sin(theta)   cos(theta) 0
+                            0            0      1];
 
-for angle = [0 theta]
+  % Rotate the [x y 1] vectors by Theta 
+  % which is done by the magic of matrix multiplication
+  rotatedSourceData= unrotatedSourceData * rotateByThetaMatrix + randomJitter;
 
-  if(angle == 0)
-    rotated_tfeatures= tfeatures + randomJitter;
-  else
-    % Rotate the [x y 1] vectors by Theta 
-    % which is done by the magic of matrix multiplication
-    rotated_tfeatures= tfeatures * rotateByThetaMatrix;
-  endif
-
-  rotated_x= rotated_tfeatures(:,1);
-  rotated_y= rotated_tfeatures(:,2);
+  rotated_x= rotatedSourceData(:,1);
+  rotated_y= rotatedSourceData(:,2);
 
 
-  % Create features for regression learning as [rotated-x rotated-x^2]
+  % Create features for regression learning
   features= [rotated_x      ...
              rotated_x.^2   ...
-             rotated_y      ...
-             rotated_y.^2   ...
-             rotated_x .* rotated_y ...
-             ones(rows(rotated_x),1) ];
+             rotated_x.^3   ...
+             ones(nRows,1) ];
+  
   X= features;
-  y= zeros(rows(rotated_x), 1);
+  y= [rotated_x rotated_y ones(nRows,1)];
 
   % Solve it in one line
   coefficients = (X' * X) \ X' * y; 
 
 
+  % Extrapolate it
+  extrapRows=20;
+  extrapInterval=linspace(-5, 7, extrapRows)';
+  curveToExtrapolate = [ extrapInterval  extrapInterval.^2 ones(extrapRows,1) ] * rotateByThetaMatrix;
+  
+  xToExtrapolate=curveToExtrapolate(:,1);  
+  extrapolatedFeatures= [xToExtrapolate ...
+                        xToExtrapolate.^2 ...
+                        xToExtrapolate.^3 ...
+                        ones(extrapRows,1) ];
+
+  extrapolatedXY1=  extrapolatedFeatures * coefficients;
+  
+  
   % Show the original points and the extrapolation
+  fprintf('\n Coefficents for theta= %d°:\n', theta * 180/pi);
   disp(coefficients);
-  figure(ifelse(angle==0, 1,2))
-  if(angle==0)
-    extrapolatedX = linspace(-4, 7, 20);
-    extrapolatedY = coefficients(1)*extrapolatedX + coefficients(2) * extrapolatedX.^2 + coefficients(3);
-    plot(extrapolatedX, extrapolatedY, '-co')
-    title('Black is original, blue is extrapolated')
-  else
-    title('Rotated quadratics aren''t that hard')
-  endif
-  hold on
-  plot(rotated_tfeatures(:,1), rotated_tfeatures(:,2), '-.k+') % data for which we found the best fit
+  figure(ifelse(theta==0, 1,2));
+  plot(rotatedSourceData(:,1), rotatedSourceData(:,2), '-.k+'); % data for which we found the best fit
+  hold on;
+  plot(extrapolatedXY1(:,1), extrapolatedXY1(:,2), '-co');
+  title(sprintf('Angle %d° Black is original, blue is extrapolated', theta * 180/pi));
   hold off
 
 endfor
