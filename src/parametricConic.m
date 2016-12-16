f39d7fc9e0f78ffb7834bd3fb9f45fe90b39220c
chrisfcarroll <chris.carroll@unforgettable.me.uk>
2016-12-16 08:07:13 +0000

    WIP parametricConic

--- src/parametricConic.m
+++ src/parametricConic.m
@@ -1,90 +1,85 @@
-% Create fake data for points approximately on a parabola which is at an angle theta from 'straight up'
-%
-
-theta= pi/5; % given theta as angle to rotate by
-
-% Matrix magic: This matrix represents a rotation through angle Theta.
-rotateByThetaMatrix= [cos(theta)  -sin(theta) 0 
-                      sin(theta)   cos(theta) 0
-                          0            0      1];
-
 %------------------------------------------------------------------------------
+%
+% Create fake data for points approximately on a parabola which is at an theta theta from 'straight up'
+%
 %{
 Example: 
-
 [x y 1]'  = [a  0  1 ;  * [t t^2 1]'
              0  b  1 ;
              0  0  1 ]
-
 and for the rotated case:
-
-[x y 1]'  = rotationMatrix * coefficentsMatrix  * [t t^2 1]'
+[x y 1]'  = coefficentsMatrix  * rotationMatrix * [t t^2 1]'
 %}
 
+  unrotatedParabola= [
+    -5  25  1
+    -4  16  1
+    -3   9  1
+    -2   4  1
+    -1   1  1
+     0   0  1
+     1   1  1
+     2   4  1
+     3   9  1
+     4  16  1
+     5  25  1
+    ];
+  randomJitter=[ rand(rows(unrotatedParabola),2) ones(rows(unrotatedParabola),1)]; % Jitter the x & y but not the 1 column
 
-% let a=b=1
+figureNumber=0;
+for theta = 0  % [0 pi/12 pi/4]
+  figureNumber++;
 
-unrotatedXYs= [
-  -3   9  1
-  -2   4  1
-  -1   1  1
-   0   0  1
-   1   1  1
-   2   4  1
-   3   9  1
-   4  16  1
-   5  25  1
-  ];
+  cTheta= cos(theta); sTheta= sin(theta); x0=10; y0=20;
+  rotateByThetaAndTranslateByX0Y0 = ...
+      [ cTheta  -sTheta  x0 
+        sTheta   cTheta  y0
+             0        0   1 ];
 
-coefficients= ones(3);
-
-tfeatures= unrotatedXYs;
-
-randomJitter=[ rand(rows(tfeatures),2) ones(rows(tfeatures),1)];
-
-for angle = [0 theta]
-
-  if(angle == 0)
-    rotated_tfeatures= tfeatures + randomJitter;
-  else
-    % Rotate the [x y 1] vectors by Theta 
-    % which is done by the magic of matrix multiplication
-    rotated_tfeatures= tfeatures * rotateByThetaMatrix;
-  endif
-
+  rotated_tfeatures= (unrotatedParabola + randomJitter) * rotateByThetaAndTranslateByX0Y0;
+ 
   rotated_x= rotated_tfeatures(:,1);
   rotated_y= rotated_tfeatures(:,2);
 
 
   % Create features for regression learning as [rotated-x rotated-x^2]
-  features= [rotated_x      ...
-             rotated_x.^2   ...
-             rotated_y      ...
-             rotated_y.^2   ...
-             rotated_x .* rotated_y ...
-             ones(rows(rotated_x),1) ];
-  X= features;
-  y= zeros(rows(rotated_x), 1);
+  % features= [rotated_x      ...
+  %            rotated_x.^2   ...
+  %            rotated_y      ...
+  %            rotated_y.^2   ...
+  %            rotated_x .* rotated_y ...
+  %            ones(rows(rotated_x),1) ];
+
+  X= rotated_tfeatures;
+  y= [rotated_x rotated_y];
 
   % Solve it in one line
   coefficients = (X' * X) \ X' * y; 
 
+  %extrapolate it
+  rangeXToExtrapolate = linspace(-4, 7, 20);
+
+  extrapolatedt1 = ifelse(sTheta==0,
+    rangeXToExtrapolate - x0,
+    (-cTheta + sqrt(cTheta^2 - 4 * sTheta * (x0 - rangeXToExtrapolate))) / 2 / sTheta
+    );
+  extrapolatedt2 = ifelse(sTheta==0,
+    rangeXToExtrapolate - x0,
+    (-cTheta - sqrt(cTheta^2 - 4 * sTheta * (x0 - rangeXToExtrapolate))) / 2 / sTheta
+    );
+
+  extrapolatedXY = coefficients(1)*extrapolatedt1 + coefficients(2) * extrapolatedt1.^2 + coefficients(3);  
+  extrapolatedX  = extrapolatedXY(:,1);
+  extrapolatedY  = extrapolatedXY(:,2);
 
   % Show the original points and the extrapolation
   disp(coefficients);
-  figure(ifelse(angle==0, 1,2))
-  if(angle==0)
-    extrapolatedX = linspace(-4, 7, 20);
-    extrapolatedY = coefficients(1)*extrapolatedX + coefficients(2) * extrapolatedX.^2 + coefficients(3);
-    plot(extrapolatedX, extrapolatedY, '-co')
-    title('Black is original, blue is extrapolated')
-  else
-    title('Rotated quadratics aren''t that hard')
-  endif
+  figure(figureNumber);
+  plot(rotated_tfeatures(:,1), rotated_tfeatures(:,2), '-.k+') % original data that we're trying to fit
   hold on
-  plot(rotated_tfeatures(:,1), rotated_tfeatures(:,2), '-.k+') % data for which we found the best fit
+  plot(extrapolatedX, extrapolatedY, '-co')
+  title( sprintf('Theta=%i deg, Black is original, blue is extrapolated', theta * 180/pi ));
   hold off
-
 endfor
 
 
@@ -97,6 +92,7 @@ Q1= [a  0  b
      0  0 -1 
      0  0  c];
 quadratic= @(xy, M)([xy 1] * M * [xy 1]'); % this calculates (ax^2 + bx + c - y)
-rotatedquadratic= @(xy, M, rotateByThetaMatrix)([xy 1] * rotateByThetaMatrix' * M * rotateByThetaMatrix * [xy 1]');
+rotatedquadratic= @(xy, M, rotateByThetaAndTranslateByX0Y0)(
+                    [xy 1] * rotateByThetaAndTranslateByX0Y0' * M * rotateByThetaAndTranslateByX0Y0 * [xy 1]');
 %
 %----------------------------------------------------------
