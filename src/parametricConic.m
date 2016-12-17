c5874740db9ddf0fcc2de1471d65b9e72f170fc9
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2016-12-17 04:06:23 +0000

    correctly draws parametric conic

--- src/parametricConic.m
+++ src/parametricConic.m
@@ -12,7 +12,6 @@ and for the rotated case:
 %}
 
   unrotatedParabola= [
-    -5  25  1
     -4  16  1
     -3   9  1
     -2   4  1
@@ -22,63 +21,59 @@ and for the rotated case:
      2   4  1
      3   9  1
      4  16  1
-     5  25  1
     ];
-  randomJitter=[ rand(rows(unrotatedParabola),2) ones(rows(unrotatedParabola),1)]; % Jitter the x & y but not the 1 column
+  randomJitter=[ rand(rows(unrotatedParabola),2)/2-0.5 zeros(rows(unrotatedParabola),1)]; % Jitter the x & y but not the 1 column
 
 figureNumber=0;
 for theta = [0 pi/12 pi/4]
   figureNumber++; fprintf('%i) Theta=%d\n', figureNumber ,theta);
   
 
-  cTheta= cos(theta); sTheta= sin(theta); x0=10; y0=20;
+  cTheta= cos(theta); sTheta= sin(theta); x0=3; y0=7;
   rotateByThetaAndTranslateByX0Y0 = ...
       [ cTheta  -sTheta  x0 
         sTheta   cTheta  y0
              0        0   1 ];
 
-  rotated_tfeatures= (unrotatedParabola + randomJitter) * rotateByThetaAndTranslateByX0Y0;
+  rotated_tfeatures= (unrotatedParabola + randomJitter) * rotateByThetaAndTranslateByX0Y0';
  
-  rotated_x= rotated_tfeatures(:,1);
-  rotated_y= rotated_tfeatures(:,2);
-
-
-  % Create features for regression learning as [rotated-x rotated-x^2]
-  % features= [rotated_x      ...
-  %            rotated_x.^2   ...
-  %            rotated_y      ...
-  %            rotated_y.^2   ...
-  %            rotated_x .* rotated_y ...
-  %            ones(rows(rotated_x),1) ];
-
   X= rotated_tfeatures;
-  y= [rotated_x rotated_y];
+  y= [rotated_tfeatures(:,1) rotated_tfeatures(:,2)];
 
   % Solve it in one line
-  coefficients = (X' * X) \ X' * y; 
+  %coefficients = (X' * X) \ X' * y; 
+  % coefficients = round(coefficients * 1e12) / 1e12;
+  % disp(coefficients);
 
   %extrapolate it
-  rangeXToExtrapolate = linspace(-4, 7, 20);
+  rangeXToExtrapolate = linspace(-20, 20, 21);
 
   extrapolatedt1 = ifelse(sTheta==0,
     rangeXToExtrapolate - x0,
-    (-cTheta + sqrt(cTheta^2 - 4 * sTheta * (x0 - rangeXToExtrapolate))) / 2 / (sTheta||1)
+    (-cTheta + sqrt(cTheta^2 - 4 * sTheta * (rangeXToExtrapolate - x0))) / 2 / sTheta
     );
   extrapolatedt2 = ifelse(sTheta==0,
     rangeXToExtrapolate - x0,
-    (-cTheta - sqrt(cTheta^2 - 4 * sTheta * (x0 - rangeXToExtrapolate))) / 2 / (sTheta||1)
+    (-cTheta - sqrt(cTheta^2 - 4 * sTheta * (rangeXToExtrapolate - x0))) / 2 / sTheta
     );
 
-  extrapolatedXY = coefficients(1)*extrapolatedt1 + coefficients(2) * extrapolatedt1.^2 + coefficients(3);  
-  extrapolatedX  = extrapolatedXY(:,1);
-  extrapolatedY  = extrapolatedXY(:,2);
+  tt = [ extrapolatedt1( imag(extrapolatedt1)==0 ) flip( extrapolatedt2( imag(extrapolatedt2)==0 ) )];
+  xx= cTheta * tt - sTheta * tt.^2 + x0;
+  yy= sTheta* tt + cTheta * tt.^2 + y0;
+
+  % sortedAndExtrapolatedXY= sortrows( [xx' yy'] );
+  % extrapolatedX= sortedAndExtrapolatedXY(:,1);
+  % extrapolatedY= sortedAndExtrapolatedXY(:,2);
+
+  extrapolatedX= xx;
+  extrapolatedY= yy;
 
   % Show the original points and the extrapolation
-  disp(coefficients);
+  fprintf('\n');
   figure(figureNumber);
-  plot(rotated_tfeatures(:,1), rotated_tfeatures(:,2), '-.k+') % original data that we're trying to fit
+  plot(rotated_tfeatures(:,1), rotated_tfeatures(:,2), '-.ko') % original data that we're trying to fit
   hold on
-  plot(extrapolatedX, extrapolatedY, '-co')
+  plot(extrapolatedX, extrapolatedY, '-cx')
   title( sprintf('Theta=%i deg, Black is original, blue is extrapolated', theta * 180/pi ));
   hold off
 
